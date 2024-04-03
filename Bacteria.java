a55c7880afd76b6f526919535a682dbe58b80e44
Anushka Dole <90973768+adole2008@users.noreply.github.com>
2024-04-03 14:19:18 -0400

    initial commit: added base files
    if my computer dies mid class we can still work on it :D

---
+++ Bacteria.java
@@ -0,0 +1,16 @@
+public class Bacteria {
+    private String name;
+    private String family;
+    private String[] whereFound = new String[0];
+    private boolean isHarmfulToHumans;
+
+    public Bacteria(String name, String family, String[] whereFound, boolean harmful)) {
+        this.
+
+t      his.family = family;
+        this.whereFound = whereFound;
+        isHarmfulToHumans = harmful;  ;em
+
+    public Bacteria(String name, String family)an = ema
+    }
+}
